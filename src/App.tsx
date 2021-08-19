e1358630ec3b9d4a73fa24691342435b3ebbc4d2
Your NameDmitryDor <you@example.com>
2021-08-19 07:44:19 +0300

    1.0 general view

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {ChangeEvent, useState, KeyboardEvent} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let [typeValue, setTypeValue] = useState<string>('')
+    let [erroe, setError] = useState<string | null>(null)
+    let [result, setResult] = useState('')
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        const newValue = e.currentTarget.value
+        setTypeValue(newValue)
+    }
+    const onClickHandler = () => {
+        setResult(typeValue)
+    }
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter") {
+            onClickHandler()
+        }
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <input type="text" value={typeValue}
+                       onChange={onChangeHandler} className="inputType"
+                       onKeyPress={onKeyPressHandler}
+                />
+                <button onClick={onClickHandler} className="buttonType">Enter</button>
+                <span className="resultType">{result.length}</span>
+            </header>
+        </div>
+    )
 }
 
 export default App;
